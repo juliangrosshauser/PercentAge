902eb453b74037bf6754542e96003c69bd1b43ee
Julian Grosshauser <julian@juliangrosshauser.com>
2015-03-14 18:19:39 +0100

    Setup window

--- Age/Classes/Application/AppDelegate.swift
+++ Age/Classes/Application/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.backgroundColor = UIColor.whiteColor()
+        self.window?.rootViewController = UINavigationController(rootViewController: AgeViewController())
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 }
