b0dda64bdb91b785fad487b3f33cf545ae432b0b
Julian Grosshauser <julian@juliangrosshauser.com>
2015-03-14 18:06:07 +0100

    Add Xcode project

---
+++ Age/Classes/View Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Age
+//
+//  Created by Julian Grosshauser on 14/03/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ AgeTests/Classes/AgeTests.swift
@@ -0,0 +1,25 @@
+//
+//  AgeTests.swift
+//  AgeTests
+//
+//  Created by Julian Grosshauser on 14/03/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AgeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(true, "Pass")
+    }
+}
