3147de8f6bc311aa8f31ea627f78a9869d90e967
Julian Grosshauser <julian@juliangrosshauser.com>
2015-03-30 23:55:21 +0200

    Replace AgeTests with AgeViewControllerSpec

---
+++ AgeTests/Classes/AgeViewControllerSpec.swift
@@ -0,0 +1,24 @@
+//
+//  AgeTests.swift
+//  AgeViewControllerSpec
+//
+//  Created by Julian Grosshauser on 14/03/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Age
+
+class AgeViewControllerSpec: QuickSpec {
+
+    override func spec() {
+        describe("an AgeViewController") {
+            var ageViewController: AgeViewController!
+
+            beforeEach {
+                ageViewController = AgeViewController()
+            }
+        }
+    }
+}
