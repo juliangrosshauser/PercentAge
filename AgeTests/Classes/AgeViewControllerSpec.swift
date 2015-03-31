bff1f02ce982a058be68b072e8142c31bd91c824
Julian Grosshauser <julian@juliangrosshauser.com>
2015-03-31 22:32:40 +0200

    Add first AgeViewController example
    Add example for `ageInPercent(birthday, today)` when birthday is today.

--- AgeTests/Classes/AgeViewControllerSpec.swift
+++ AgeTests/Classes/AgeViewControllerSpec.swift
@@ -13,11 +13,36 @@ import Age
 class AgeViewControllerSpec: QuickSpec {
 
     override func spec() {
-        describe("an AgeViewController") {
+        var dateFormatter: NSDateFormatter!
+
+        beforeSuite {
+            dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "dd-MM-yyyy"
+        }
+
+        describe("AgeViewController") {
             var ageViewController: AgeViewController!
 
             beforeEach {
                 ageViewController = AgeViewController()
+            }
+
+            describe("ageInPercent(birthday, today)") {
+                var birthday: NSDate!
+                var today: NSDate!
+
+                context("when birthday is today") {
+                    beforeEach {
+                        birthday = dateFormatter.dateFromString("06-03-1991")
+                        today = dateFormatter.dateFromString("06-03-2015")
+                    }
+                    
+                    it("returns whole number") {
+                        let ageInPercent = ageViewController.ageInPercent(birthday: birthday, today: today)
+                        let ageInPercentRoundedString = NSString(format: "%.2f", ageInPercent)
+                        expect(ageInPercentRoundedString).to(equal("24.00"))
+                    }
+                }
             }
         }
     }
