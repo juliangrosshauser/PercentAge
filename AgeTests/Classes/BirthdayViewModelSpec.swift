e8fcb9176b806b8787aeb280de5bb890ddb13e3e
Julian Grosshauser <julian@juliangrosshauser.com>
2015-04-09 23:38:59 +0200

    Set up BirthdayViewModelSpec

---
+++ AgeTests/Classes/BirthdayViewModelSpec.swift
@@ -0,0 +1,24 @@
+//
+//  BirthdayViewModelSpec.swift
+//  AgeTests
+//
+//  Created by Julian Grosshauser on 09/04/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Age
+
+class BirthdayViewModelSpec: QuickSpec {
+
+    override func spec() {
+        describe("BirthdayViewModel") {
+            var birthdayViewModel: BirthdayViewModel!
+
+            beforeEach {
+                birthdayViewModel = BirthdayViewModel()
+            }
+        }
+    }
+}
