e9f3c5d7a8728eed63ca0eb7c80e0599cf731cac
Julian Grosshauser <julian@juliangrosshauser.com>
2015-04-10 23:45:40 +0200

    Add examples for incrementDay and decrementDay

--- AgeTests/Classes/BirthdayViewModelSpec.swift
+++ AgeTests/Classes/BirthdayViewModelSpec.swift
@@ -14,10 +14,40 @@ class BirthdayViewModelSpec: QuickSpec {
 
     override func spec() {
         describe("BirthdayViewModel") {
-            var birthdayViewModel: BirthdayViewModel!
+            describe("incrementDay") {
+                context("day value is at bottom limit") {
+                    it("increments day value") {
+                        let birthdayViewModel = BirthdayViewModel(day: 1, month: 1, year: 2015)
+                        birthdayViewModel.incrementDay()
+                        expect(birthdayViewModel.day).to(equal(2))
+                    }
+                }
 
-            beforeEach {
-                birthdayViewModel = BirthdayViewModel()
+                context("day value is at upper limit") {
+                    it("sets day value to bottom limit") {
+                        let birthdayViewModel = BirthdayViewModel(day: 31, month: 1, year: 2015)
+                        birthdayViewModel.incrementDay()
+                        expect(birthdayViewModel.day).to(equal(1))
+                    }
+                }
+            }
+
+            describe("decrementDay") {
+                context("day value is at bottom limit") {
+                    it("sets day value to upper limit") {
+                        let birthdayViewModel = BirthdayViewModel(day: 1, month: 1, year: 2015)
+                        birthdayViewModel.decrementDay()
+                        expect(birthdayViewModel.day).to(equal(31))
+                    }
+                }
+
+                context("day value is at upper limit") {
+                    it("decrements day value") {
+                        let birthdayViewModel = BirthdayViewModel(day: 31, month: 1, year: 2015)
+                        birthdayViewModel.decrementDay()
+                        expect(birthdayViewModel.day).to(equal(30))
+                    }
+                }
             }
         }
     }
