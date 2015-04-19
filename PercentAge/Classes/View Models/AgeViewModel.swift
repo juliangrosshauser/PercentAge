ceb077d1ded10646129ba034e5c49a68a8764189
Julian Grosshauser <julian@juliangrosshauser.com>
2015-04-19 22:56:10 +0200

    Init `calendar` as implicitly unwrapped optional

--- PercentAge/Classes/View Models/AgeViewModel.swift
+++ PercentAge/Classes/View Models/AgeViewModel.swift
@@ -18,23 +18,23 @@ public class AgeViewModel {
     //MARK: Date Calculations
 
     public func ageInPercent(#birthday: NSDate, today: NSDate) -> Float {
-        let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+        let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
 
-        let currentYear = calendar!.component(NSCalendarUnit.CalendarUnitYear, fromDate: today)
-        let birthdayDay = calendar!.component(NSCalendarUnit.CalendarUnitDay, fromDate: birthday)
-        let birthdayMonth = calendar!.component(NSCalendarUnit.CalendarUnitMonth, fromDate: birthday)
+        let currentYear = calendar.component(NSCalendarUnit.CalendarUnitYear, fromDate: today)
+        let birthdayDay = calendar.component(NSCalendarUnit.CalendarUnitDay, fromDate: birthday)
+        let birthdayMonth = calendar.component(NSCalendarUnit.CalendarUnitMonth, fromDate: birthday)
 
         let birthdayCurrentYearComponents = NSDateComponents()
         birthdayCurrentYearComponents.day = birthdayDay
         birthdayCurrentYearComponents.month = birthdayMonth
         birthdayCurrentYearComponents.year = currentYear
 
-        let birthdayCurrentYear = calendar!.dateFromComponents(birthdayCurrentYearComponents)!
+        let birthdayCurrentYear = calendar.dateFromComponents(birthdayCurrentYearComponents)!
 
-        let yearDifferenceComponent = calendar?.components(NSCalendarUnit.CalendarUnitYear, fromDate: birthday, toDate: today, options: NSCalendarOptions.allZeros)
-        let years = yearDifferenceComponent!.year
+        let yearDifferenceComponent = calendar.components(NSCalendarUnit.CalendarUnitYear, fromDate: birthday, toDate: today, options: NSCalendarOptions.allZeros)
+        let years = yearDifferenceComponent.year
 
-        if (calendar!.isDate(birthdayCurrentYear, inSameDayAsDate: today)) {
+        if (calendar.isDate(birthdayCurrentYear, inSameDayAsDate: today)) {
             return Float(years)
         }
 
@@ -49,7 +49,7 @@ public class AgeViewModel {
             birthdayLastYearComponents.month = birthdayMonth
             birthdayLastYearComponents.year = currentYear - 1
 
-            let birthdayLastYear = calendar!.dateFromComponents(birthdayLastYearComponents)!
+            let birthdayLastYear = calendar.dateFromComponents(birthdayLastYearComponents)!
 
             let daysBetweenBirthdays = self.dayDifference(before: birthdayLastYear, after: birthdayCurrentYear)
             let daysSinceBirthday = self.dayDifference(before: birthdayLastYear, after: today)
@@ -62,7 +62,7 @@ public class AgeViewModel {
             birthdayNextYearComponents.month = birthdayMonth
             birthdayNextYearComponents.year = currentYear + 1
 
-            let birthdayNextYear = calendar!.dateFromComponents(birthdayNextYearComponents)!
+            let birthdayNextYear = calendar.dateFromComponents(birthdayNextYearComponents)!
 
             let daysBetweenBirthdays = self.dayDifference(before: birthdayCurrentYear, after: birthdayNextYear)
             let daysSinceBirthday = self.dayDifference(before: birthdayCurrentYear, after: today)
