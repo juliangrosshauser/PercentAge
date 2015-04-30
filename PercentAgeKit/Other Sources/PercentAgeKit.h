af7d669d2c6a323941ab913ae9705f147fb0a0a9
Julian Grosshauser <julian@juliangrosshauser.com>
2015-04-30 23:25:01 +0200

    Create PercentAgeKit framework

---
+++ PercentAgeKit/Other Sources/PercentAgeKit.h
@@ -0,0 +1,15 @@
+//
+//  PercentAgeKit.h
+//  PercentAgeKit
+//
+//  Created by Julian Grosshauser on 30/04/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PercentAgeKit.
+FOUNDATION_EXPORT double PercentAgeKitVersionNumber;
+
+//! Project version string for PercentAgeKit.
+FOUNDATION_EXPORT const unsigned char PercentAgeKitVersionString[];
---
+++ PercentAgeKitTests/Classes/PercentAgeKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  PercentAgeKitTests.swift
+//  PercentAgeKitTests
+//
+//  Created by Julian Grosshauser on 30/04/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PercentAgeKitTests: XCTestCase {
+    
+    func testExample() {
+        XCTAssert(true, "Pass")
+    }
+}
